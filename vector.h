2851ea7bbb08a64eab09fddf8f00cd0edb0a913b
Jacob Clark <jacob.clark@coop.co.uk>
2017-07-23 20:57:30 +0100

    Cleaning up headers

--- vector.h
+++ vector.h
@@ -10,15 +10,16 @@ typedef struct {
 void init(Vector *vector);
 void append(Vector *vector, int value);
 void prepend(Vector *vector, int value);
-int pop(Vector *vector);
 void delete(Vector *vector, int index);
 void delete_value(Vector *vector, int value);
-int find_value(Vector *vector, int value);
 void set(Vector *vector, int index, int value);
 void resize(Vector *vector);
-int size(Vector *vector);
-int capacity(Vector *vector);
-bool is_empty(Vector *vector);
 void free_memory(Vector *vector);
 
 int get(Vector *vector, int value);
+int pop(Vector *vector);
+int find_value(Vector *vector, int value);
+int size(Vector *vector);
+int capacity(Vector *vector);
+
+bool is_empty(Vector *vector);
